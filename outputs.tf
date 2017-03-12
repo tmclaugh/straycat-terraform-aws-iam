cb6f241544f007703230cf4197618ceea3ab29db
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-03-12 18:56:40 -0400

    Output renames

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@
 
 # aws_iam_account_password_policy exports nothing worthwhile.
 
-output "aws_key_pair_admin_key_key_name" {
+output "aws_key_pair_key_name_admin_key" {
   value = "${aws_key_pair.admin_key.key_name}"
 }
 
-output "aws_key_pair_admin_key_fingerprint" {
+output "aws_key_pair_fingerprint_admin_key" {
   value = "${aws_key_pair.admin_key.fingerprint}"
 }
 
