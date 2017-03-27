b2d3ffe2941172d513cd009908b46f0f7b79752d
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-03-27 15:22:50 -0400

    Refactor to reduce outputs

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,7 @@
 
 # aws_iam_account_password_policy exports nothing worthwhile.
 
-output "aws_key_pair_key_name_admin_key" {
-  value = "${aws_key_pair.admin_key.key_name}"
-}
-
-output "aws_key_pair_fingerprint_admin_key" {
-  value = "${aws_key_pair.admin_key.fingerprint}"
+output "aws_key_pair_admin_key" {
+  value = "${map(aws_key_pair.admin_key.key_name, aws_key_pair.admin_key.fingerprint)}"
 }
 
